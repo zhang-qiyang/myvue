c11331619b8931e59a76a6c8137e4039e94b4c47
zhang-qiyang <17796859556@163.com>
2018-11-19 14:41:47 +0800

    值传递

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -23,6 +23,30 @@
         // this.title="changed"
         this.$emit("titleChange","子向父组件传值")
       }
+    },
+    beforeCreate:function(){
+      alert("组件实例化之前执行的函数");
+    },
+    created:function(){
+      alert("组件实例化完毕，但页面还未显示");
+    },
+    beforeMount:function () {
+      alert("组件挂载前，但页面仍未显示，虚拟Dom已经配置");
+    },
+    mounted:function () {
+      alert("组件挂载后，执行此方法，页面显示");
+    },
+    beforeUpdate:function () {
+      alert("组件更新前，页面仍未更新，但虚拟Dom已经配置");
+    },
+    updated:function () {
+      alert("组件更新，执行此方法后，页面更新");
+    },
+    beforeDestroy:function () {
+      alert("组件销毁前");
+    },
+    destroyed:function () {
+      alert("组件销毁");
     }
 
   }
